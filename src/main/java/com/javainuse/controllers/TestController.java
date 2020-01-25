734e3f3e605aab44ac288bdc8cd86c74031e43c6
anunarayan <anunarayan@USHYDANUNARAYA9.us.deloitte.com>
2020-01-25 17:17:26 +0530

    ..

--- src/main/java/com/javainuse/controllers/TestController.java
+++ src/main/java/com/javainuse/controllers/TestController.java
@@ -1,17 +1,17 @@
 package com.javainuse.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javainuse.model.Employee;
 
 @RestController
+@RequestMapping(value = "/")
 public class TestController {
-
-	@GetMapping(value = "/")
+	@RequestMapping(value = "/")
 	public Employee firstPage() {
 
+		
 		Employee emp = new Employee();
 		emp.setName("emp1");
 		emp.setDesignation("manager");
