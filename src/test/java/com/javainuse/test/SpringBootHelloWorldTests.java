09842c8f8efcca775d3900311e1aae7b0fc5f372
Anujnarayan23 <anujnarayan11@gmail.com>
2020-07-05 08:49:53 +0530

    c

--- src/test/java/com/javainuse/test/SpringBootHelloWorldTests.java
+++ src/test/java/com/javainuse/test/SpringBootHelloWorldTests.java
@@ -1,5 +1,6 @@
-/*package com.javainuse.test;
+package com.javainuse.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,4 +14,3 @@ public class SpringBootHelloWorldTests {
 	}
 
 }
-*/
